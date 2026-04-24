97a71c033418fb73e67871abb7f966d6e541ef3a
Ben Edwards <ben@edwards.nz>
2026-04-24 12:07:16 +1200

    Fix URLs

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 @app.route("/register-product", methods=["GET"])
 @app.route("/register-product/", methods=["GET"])
 def register_product():
-    return redirect("https://edwardsandco.nz/register-product", code=301)
+    return redirect("https://edwardsandco.nz/pages/register-product", code=301)
 
 
 @app.route("/", defaults={"path": ""}, methods=["GET"])
 @app.route("/<path:path>", methods=["GET"])
 def catch_all(path):
-    return redirect("https://edwardsandco.nz/support", code=301)
+    return redirect("https://edwardsandco.nz/pages/support", code=301)
